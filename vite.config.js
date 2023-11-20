09616a48ca20424578eb4cc9c2accf3b532d45ec
匿名 <148196879+aro0176@users.noreply.github.com>
2023-11-21 01:02:49 +0300

    demo react

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],  server: {
     proxy: {
       '/api': {
-        target: 'https://api.apmf.com', // Remplacez par l'URL de votre serveur d'API
+        target: 'https://demoapi.apmf.com', // Remplacez par l'URL de votre serveur d'API
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
